87b76099f7d6e389d699fd78b6b359e5fe96d3fc
Sridhar <sridhar.s@314ecorp.com>
2024-05-28 18:06:39 +0530

    fixed keycloak realm settings

--- app/cli/common/keycloakUtils.py
+++ app/cli/common/keycloakUtils.py
@@ -41,16 +41,13 @@ class KeycloakAdminClient:
         """
         return self.kc_client.get_realm(realm_name)
 
-    def create_realm(self: "KeycloakAdminClient", realm_config: dict) -> None:
+    def create_realm(self: "KeycloakAdminClient", realm_config: dict, skip_exists=True) -> None:
         """
         Create keycloak realm
         """
-        try:
-            self.kc_client.connection.refresh_token()
-            self.kc_client.create_realm(payload=realm_config, skip_exists=True)
-        except Exception as e:
-            logger.error(f"Error creating realm: {e}")
-            raise
+
+        self.kc_client.connection.refresh_token()
+        self.kc_client.create_realm(payload=realm_config, skip_exists=skip_exists)
 
     def delete_realm(self: "KeycloakAdminClient", realm_name: str) -> None:
         """
