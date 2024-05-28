d2fa0a187538eb9e92f1a5e9a110563a63d1f783
Sridhar <sridhar.s@314ecorp.com>
2024-05-28 17:01:12 +0530

    added view access to matomo

--- app/cli/jeeves/postgresSetup.py
+++ app/cli/jeeves/postgresSetup.py
@@ -76,6 +76,13 @@ async def setup_postgres(jeeves: JeevesSpec):
             table="matomo_log_link_visit_action", username=db_username
         )
 
+        await postgres_utils.grant_user_all_privileges_on_table(table="matomo_log_visit_view", username=db_username)
+        await postgres_utils.grant_user_all_privileges_on_table(table="matomo_log_action_view", username=db_username)
+        await postgres_utils.grant_user_all_privileges_on_table(table="matomo_log_media_view", username=db_username)
+        await postgres_utils.grant_user_all_privileges_on_table(
+            table="matomo_log_link_visit_action_view", username=db_username
+        )
+
         return
     except Exception as e:
         logger.error(f"Error in postgres database setup: {e}")
