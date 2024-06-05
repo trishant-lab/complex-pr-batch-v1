5864e69df9ead439669ac7d1b55775c70dce3f2b
Sridhar <sridhar.s@314ecorp.com>
2024-06-05 16:00:59 +0530

    updated

--- app/cli/temporal/jeeves/workflows/onboarding.py
+++ app/cli/temporal/jeeves/workflows/onboarding.py
@@ -52,178 +52,178 @@ class JeevesOnboardingWorkflow(Workflow):
         """
 
         try:
-            print("Running workflow")
+
             await workflow.wait_condition(lambda: self.approved)
-            print("Approved")
-            #
-            # await workflow.execute_activity(
-            #     activity=PostgresSetupActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=PostgresSetupActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # namespace setup
-            # await workflow.execute_activity(
-            #     activity=NamespaceSetupActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=NamespaceSetupActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # secret setup
-            # await workflow.execute_activity(
-            #     activity=SecretSetupActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=SecretSetupActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # chatwoot setup
-            # await workflow.execute_activity(
-            #     activity=ChatwootSetupActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=ChatwootSetupActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # novu setup
-            # await workflow.execute_activity(
-            #     activity=NovuSetupActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=NovuSetupActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # statefulset setup
-            # await workflow.execute_activity(
-            #     activity=StateFullSetSetupActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=StateFullSetSetupActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # configmap setup
-            # await workflow.execute_activity(
-            #     activity=ConfigmapSetupActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=ConfigmapSetupActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # pvc setup
-            # await workflow.execute_activity(
-            #     activity=PVCSetupActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=PVCSetupActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # dns setup
-            # await workflow.execute_activity(
-            #     activity=DnsSetupActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=DnsSetupActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=600),
-            # )
-            #
-            # # ui setup
-            # await workflow.execute_activity(
-            #     activity=UiSetupActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=UiSetupActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # keycloak realm setup
-            # await workflow.execute_activity(
-            #     activity=KeycloakRealmSetupActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=KeycloakRealmSetupActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # Provisioning Job
-            # await workflow.execute_activity(
-            #     activity=ProvisioningJobActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=ProvisioningJobActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # Kubernetes Service
-            # await workflow.execute_activity(
-            #     activity=KubernetesServiceActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=KubernetesServiceActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # Kubernetes Virtual Service
-            # await workflow.execute_activity(
-            #     activity=KubernetesVirtualServiceActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=KubernetesVirtualServiceActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # Deploy server and cli
-            # await workflow.execute_activity(
-            #     activity=DeploymentActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=DeploymentActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # Vm Pod Scraper
-            # await workflow.execute_activity(
-            #     activity=VmPodScraperActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=VmPodScraperActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # Grafana Alerts
-            # await workflow.execute_activity(
-            #     activity=GrafanaAlertsActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=GrafanaAlertsActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # Temporal Namespace Creation
-            # await workflow.execute_activity(
-            #     activity=TemporalNamespaceCreationActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=TemporalNamespaceCreationActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # Ai Voice Setup
-            # await workflow.execute_activity(
-            #     activity=AiVoiceSetupActivity.defn,
-            #     arg=jeeves,
-            #     retry_policy=AiVoiceSetupActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
-            #
-            # # Update Tenant Status
-            # await workflow.execute_activity(
-            #     activity=UpdateTenantStatusActivity.defn,
-            #     arg=TenantStatus(
-            #         tenant_name=pydash.get(jeeves, 'tenant'),
-            #         status="Completed"
-            #     ),
-            #     retry_policy=UpdateTenantStatusActivity.get_retry_policy(),
-            #     start_to_close_timeout=timedelta(seconds=120),
-            # )
+
+            await workflow.execute_activity(
+                activity=PostgresSetupActivity.defn,
+                arg=jeeves,
+                retry_policy=PostgresSetupActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # namespace setup
+            await workflow.execute_activity(
+                activity=NamespaceSetupActivity.defn,
+                arg=jeeves,
+                retry_policy=NamespaceSetupActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # secret setup
+            await workflow.execute_activity(
+                activity=SecretSetupActivity.defn,
+                arg=jeeves,
+                retry_policy=SecretSetupActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # chatwoot setup
+            await workflow.execute_activity(
+                activity=ChatwootSetupActivity.defn,
+                arg=jeeves,
+                retry_policy=ChatwootSetupActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # novu setup
+            await workflow.execute_activity(
+                activity=NovuSetupActivity.defn,
+                arg=jeeves,
+                retry_policy=NovuSetupActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # statefulset setup
+            await workflow.execute_activity(
+                activity=StateFullSetSetupActivity.defn,
+                arg=jeeves,
+                retry_policy=StateFullSetSetupActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # configmap setup
+            await workflow.execute_activity(
+                activity=ConfigmapSetupActivity.defn,
+                arg=jeeves,
+                retry_policy=ConfigmapSetupActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # pvc setup
+            await workflow.execute_activity(
+                activity=PVCSetupActivity.defn,
+                arg=jeeves,
+                retry_policy=PVCSetupActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # dns setup
+            await workflow.execute_activity(
+                activity=DnsSetupActivity.defn,
+                arg=jeeves,
+                retry_policy=DnsSetupActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=600),
+            )
+
+            # ui setup
+            await workflow.execute_activity(
+                activity=UiSetupActivity.defn,
+                arg=jeeves,
+                retry_policy=UiSetupActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # keycloak realm setup
+            await workflow.execute_activity(
+                activity=KeycloakRealmSetupActivity.defn,
+                arg=jeeves,
+                retry_policy=KeycloakRealmSetupActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # Provisioning Job
+            await workflow.execute_activity(
+                activity=ProvisioningJobActivity.defn,
+                arg=jeeves,
+                retry_policy=ProvisioningJobActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # Kubernetes Service
+            await workflow.execute_activity(
+                activity=KubernetesServiceActivity.defn,
+                arg=jeeves,
+                retry_policy=KubernetesServiceActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # Kubernetes Virtual Service
+            await workflow.execute_activity(
+                activity=KubernetesVirtualServiceActivity.defn,
+                arg=jeeves,
+                retry_policy=KubernetesVirtualServiceActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # Deploy server and cli
+            await workflow.execute_activity(
+                activity=DeploymentActivity.defn,
+                arg=jeeves,
+                retry_policy=DeploymentActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # Vm Pod Scraper
+            await workflow.execute_activity(
+                activity=VmPodScraperActivity.defn,
+                arg=jeeves,
+                retry_policy=VmPodScraperActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # Grafana Alerts
+            await workflow.execute_activity(
+                activity=GrafanaAlertsActivity.defn,
+                arg=jeeves,
+                retry_policy=GrafanaAlertsActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # Temporal Namespace Creation
+            await workflow.execute_activity(
+                activity=TemporalNamespaceCreationActivity.defn,
+                arg=jeeves,
+                retry_policy=TemporalNamespaceCreationActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # Ai Voice Setup
+            await workflow.execute_activity(
+                activity=AiVoiceSetupActivity.defn,
+                arg=jeeves,
+                retry_policy=AiVoiceSetupActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
+
+            # Update Tenant Status
+            await workflow.execute_activity(
+                activity=UpdateTenantStatusActivity.defn,
+                arg=TenantStatus(
+                    tenant_name=pydash.get(jeeves, 'tenant'),
+                    status="Completed"
+                ),
+                retry_policy=UpdateTenantStatusActivity.get_retry_policy(),
+                start_to_close_timeout=timedelta(seconds=120),
+            )
         except Exception as e:
             workflow.logger.error(f"Error in onboarding workflow: {e}")
             await workflow.execute_activity(
                 activity=UpdateTenantStatusActivity.defn,
                 arg=TenantStatus(
                     tenant_name=pydash.get(jeeves, 'tenant'),
-                    status="Failed"
+                    status="Failed",
+                    error_msg=str(e)
                 ),
                 retry_policy=UpdateTenantStatusActivity.get_retry_policy(),
                 start_to_close_timeout=timedelta(seconds=120),
