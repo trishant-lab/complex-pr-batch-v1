a517a2bb332bb180957d6a4c5a5c1a3defced013
Sridhar <sridhar.s@314ecorp.com>
2024-07-29 13:26:28 +0530

    updated edit tenantdetails api

--- app/routes/tenant.py
+++ app/routes/tenant.py
@@ -14,7 +14,6 @@ from app.models.product import ProductEnum
 from app.models.tenant import (
     TenantCreateRequestModel,
     TenantResponseModel,
-    UpdateRequestorModel,
 )
 
 tenant_router = APIRouter()
@@ -138,21 +137,23 @@ async def get_tenant(tenant_id: uuid.UUID, _param: dict = Depends(get_oauth_sche
 
 
 @tenant_router.put(
-    "/updateRequestorDetails",
-    operation_id="updateRequestorDetails",
+    "/updateTenantDetails",
+    operation_id="updateTenantDetails",
 )
-async def update_tenant(requestor_details: UpdateRequestorModel, _param: dict = Depends(get_oauth_scheme())) -> dict:
+async def update_tenant(tenant_id: str, tenant_details: dict, _param: dict = Depends(get_oauth_scheme())) -> dict:
     """
-    @param requestor_details:
+    @param tenant_id:
+    @param tenant_details:
     @param _param:
     @return:
     """
     config: AppSettings = get_settings()
     try:
         db: DBManager = await get_db_manager(config.postgres.dsn)
-        response = await db.fetch_one("updateRequestor.sql", **requestor_details.dict())
+        tenant_details = orjson.dumps(tenant_details).decode("utf-8")
+        response = await db.fetch_one("updateTenantDetails.sql", schema_=tenant_details, tenant_id=tenant_id)
 
-        return {"message": "Requestor details updated successfully", "data": response}
+        return {"message": "Tenant details are updated successfully", "data": response}
 
     except Exception as e:
         logger.error(f"Error updating tenant: {e}")
--- app/sql/updateRequestor.sql
+++
@@ -1,5 +0,0 @@
-UPDATE requestor
-SET username = {{username}},
-    email = {{email}},
-    organization = {{organization}}
-WHERE requestor.id = (SELECT requestor from tenant where id = {{tenant_id}})
