174838258d3e47ed6c659c64ef830f4ff6569be4
Sridhar <sridhar.s@314ecorp.com>
2024-08-06 15:57:05 +0530

    updated error msg

--- app/routes/provisioning.py
+++ app/routes/provisioning.py
@@ -136,7 +136,7 @@ async def prepare_schema(product: ProductEnum, schema: dict) -> dict:
     if existing_tenant_names:
         raise HTTPException(
             status_code=HTTP_400_BAD_REQUEST,
-            detail=f"{product.value} Environment Already Exist with this email domain '{company_domain}'",
+            detail="An active free trial exists for your organization. Please contact the admin to gain access.",
         )
 
     if not company_domain[0].isdigit():
