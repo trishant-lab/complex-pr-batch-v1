be3ba3caf4a2962ae2d6076d7831e6500f274b30
Sridhar <sridhar.s@314ecorp.com>
2024-11-07 18:03:51 +0530

    update dexit workflow

--- app/cli/temporal/dexit/workflows/onboarding.py
+++ app/cli/temporal/dexit/workflows/onboarding.py
@@ -508,6 +508,7 @@ class DexitOnboardingWorkflow(Workflow):
                     src_object_name=src_object_name,
                     dest_dir=dest_dir,
                     bundle_path=bundle_path,
+                    bundle_name="bundle.zip",
                 ),
                 retry_policy=UiSetupActivity.get_retry_policy(),
                 start_to_close_timeout=UiSetupActivity.get_timeout(),
@@ -652,7 +653,7 @@ class DexitOnboardingWorkflow(Workflow):
                         {"name": "POSTGRES_USER", "value": postgres_username},
                         {"name": "RELEASE_VERSION", "value": image_tag},
                         {"name": "TIKA_SERVER_ENDPOINT", "value": dexit_config.tika_server_endpoint},
-                        {"name": "CLI", "value": False},
+                        {"name": "CLI", "value": "FALSE"},
                     ],
                 ),
                 retry_policy=KubernetesStatefulSetActivity.get_retry_policy(),
@@ -716,7 +717,7 @@ class DexitOnboardingWorkflow(Workflow):
                         {"name": "POSTGRES_USER", "value": postgres_username},
                         {"name": "RELEASE_VERSION", "value": image_tag},
                         {"name": "TIKA_SERVER_ENDPOINT", "value": dexit_config.tika_server_endpoint},
-                        {"name": "CLI", "value": True},
+                        {"name": "CLI", "value": "TRUE"},
                     ],
                 ),
                 retry_policy=KubernetesStatefulSetActivity.get_retry_policy(),
@@ -738,7 +739,7 @@ class DexitOnboardingWorkflow(Workflow):
                         "cpu": pydash.get(dexit, "serverSpec.limit_cpu"),
                         "memory": pydash.get(dexit, "serverSpec.limit_memory"),
                     },
-                    container_ports=[8000],
+                    container_ports=[],
                     volume_mounts=[
                         {
                             "name": "dicom-volume",
@@ -764,6 +765,18 @@ class DexitOnboardingWorkflow(Workflow):
                 ),
                 retry_policy=KubernetesStatefulSetActivity.get_retry_policy(),
                 start_to_close_timeout=KubernetesStatefulSetActivity.get_timeout(),
+            )
+
+            # kubernetes service for dicom
+            await workflow.execute_activity(
+                activity=KubernetesServiceActivity.defn,
+                arg=KubernetesServiceActivityModel(
+                    namespace=tenant,
+                    service_name="dexit-dicom",
+                    port=8042,
+                ),
+                retry_policy=KubernetesServiceActivity.get_retry_policy(),
+                start_to_close_timeout=KubernetesServiceActivity.get_timeout(),
             )
 
             # vm pod scraper
