8b0c3a1cdde399985a7b162e21e4224ac1d937ec
satwik.mishra <satwik.mishra@314ecorp.com>
2024-12-27 14:14:45 +0530

    changes

--- app/cli/temporal/zsegment/workflows/onboarding.py
+++ app/cli/temporal/zsegment/workflows/onboarding.py
@@ -292,11 +292,7 @@ class ZSegmentOnboardingWorkflow(Workflow):
                 start_to_close_timeout=OnePasswordGetActivity.get_timeout(),
             )
 
-            installer_secret = (
-                generate_password(length=20)
-                if not installer_secret
-                else installer_secret
-            )
+            installer_secret = generate_password(length=20) if not installer_secret else installer_secret
 
             await workflow.execute_activity(
                 activity=OnePasswordCreateOrUpdateActivity.defn,
@@ -947,9 +943,7 @@ class ZSegmentOnboardingWorkflow(Workflow):
             # update tenant status
             await workflow.execute_activity(
                 activity=UpdateTenantStatusActivity.defn,
-                arg=TenantStatus(
-                    tenant_name=tenant, status="Completed", product=ProductName
-                ),
+                arg=TenantStatus(tenant_name=tenant, status="Completed", product=ProductName),
                 retry_policy=UpdateTenantStatusActivity.get_retry_policy(),
                 start_to_close_timeout=UpdateTenantStatusActivity.get_timeout(),
             )
