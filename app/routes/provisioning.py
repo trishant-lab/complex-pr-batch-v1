6e0b38479c14f0bbab52ef7ada25b0519e969568
nikhil-kumar-03 <139747998+nikhil-kumar-03@users.noreply.github.com>
2025-03-27 16:42:11 +0530

    Fix: change approvedby to approvedBy in approve/decline api (#377)
    * make email optional in TenantResponseModel and ProductResponseModel
    
    * change attribute name from snake case to camel case
    
    * refactor provisionig api
    
    * fix approve or decline api

--- app/routes/provisioning.py
+++ app/routes/provisioning.py
@@ -183,7 +183,7 @@ async def approve_tenant(
 
     db: DBManager = await get_db_manager()
     response = await db.fetch_one("get_tenant.sql", tenant_id=str(tenant_id))  # NOSONAR
-    tenant_params = {"table": "customer", "payload": {"approvedBy": user_id}, "where": f"id='{tenant_id!s}'"}
+    tenant_params = {"table": "customer", "payload": {'"approvedBy"': user_id}, "where": f"id='{tenant_id!s}'"}
     operator_params = {
         "table": "provisioningstatus",
         "payload": {"status": TenantStatusEnum.Provisioning if approval else TenantStatusEnum.Declined},
