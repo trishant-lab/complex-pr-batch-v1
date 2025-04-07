4d26be02eb48c6d7fb1ad7334064cb9d7bac9ace
Nikhil Kumar <nikhil.kumar@314ecorp.com>
2025-04-07 15:56:30 +0530

    fix cloudflare bucket dir in veritable onboarding workflow

--- app/cli/temporal/veritable/workflows/onboarding.py
+++ app/cli/temporal/veritable/workflows/onboarding.py
@@ -482,11 +482,7 @@ class VeritableOnboardingWorkflow(Workflow):
             repo_name = "veritable-ui"
             image_tag = "production" if config.env == "production" else "sprint"
 
-            if config.env == "production":
-                dest_dir = ui_bucket
-            else:
-                dest_dir = f"{ui_bucket}/{image_tag}"
-
+            dest_dir = ui_bucket
             src_object_name = f"{repo_name}/{image_tag}/bundle.zip"
 
             bundle_path = "bundle/dist"
