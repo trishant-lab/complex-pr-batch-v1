541fcdc9edf2dd7ef34df380a142354d778413b9
Satish Kumar <101244993+kumarsatish29@users.noreply.github.com>
2025-08-06 18:18:35 +0530

    add veritable image tag for testing (#620)

--- app/cli/temporal/veritable/workflows/onboarding.py
+++ app/cli/temporal/veritable/workflows/onboarding.py
@@ -573,7 +573,7 @@ class VeritableOnboardingWorkflow(Workflow):
                 ),
             )
 
-            image_tag = "veritable-latest" if config.env == "production" else "sprint"
+            image_tag = "veritable-latest" if config.env == "production" else "sprint-testing"
             docker_image = f"registry.314ecorp.tech/veritable-app:{image_tag}"
             # provisioning job
             await run_activity(
