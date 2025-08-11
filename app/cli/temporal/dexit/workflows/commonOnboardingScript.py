a50373b924b2606ddd6f6fbb42bdb0e7dcb84d19
Chirag Bohra <chirag.bohra@314ecorp.com>
2025-08-11 17:05:57 +0530

    aprroval of dexit implemented

--- app/cli/temporal/dexit/workflows/commonOnboardingScript.py
+++ app/cli/temporal/dexit/workflows/commonOnboardingScript.py
@@ -448,8 +448,10 @@ class DexitCommonOnboardingWorkflow(Workflow):
             )
 
             # Create Service account
-            client_secret = await run_activity(activity=CreatePasswordActivity, 
-                                               arg=CreatePasswordActivityModel(length=32))
+            client_secret = await run_activity(
+                activity=CreatePasswordActivity,
+                arg=CreatePasswordActivityModel(length=32),
+            )
 
             await run_activity(
                 activity=OnePasswordCreateOrUpdateActivity,
