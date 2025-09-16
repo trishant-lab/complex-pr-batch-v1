62907bb490b53ac89ad8c8ddfa5584d6241c718b
Yogesh <63769109+kyogesha@users.noreply.github.com>
2025-09-16 15:35:34 +0530

    Update get_tenant_link.sql

--- app/sql/get_tenant_link.sql
+++ app/sql/get_tenant_link.sql
@@ -1,4 +1,4 @@
-select concat({{env_prefix}}::text, tenantname, {{ env_suffix }}::text) as tenantlinks
+select concat({{env_prefix}}::text, tenantname, '.', {{ env_suffix }}::text) as tenantlinks
 from customer c
 where id in (select customerid from userevent where email=LOWER({{ email }}) and status) or email=LOWER({{ email }})
 and c.product = LOWER({{ product }})
